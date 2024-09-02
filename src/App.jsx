a24ee7a4489c0cf36d40cd13f682bd65a805ee22
vjnnxx <florianovinicius09@gmail.com>
2024-09-02 16:45:06 -0300

    Resolvendo chamadas da api

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect} from 'react'
 import './App.css'
+import Card from './Card';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(()=>{
+
+    const numbers = [];
+
+    async function getPokemon(url){
+      return await fetch(url)
+      .then(response => response.json())
+      .then(data => data)
+      .catch(error => console.error('Erro:',error));
+    }
+
+    for(let i = 0; i < 10; i++){
+      let randomNumber = parseInt((Math.random() * 100) + 1);
+
+      numbers.push(randomNumber);
+      
+    } 
+
+    let promises = numbers.map((el)=>{
+      const apiCall = getPokemon(`https://pokeapi.co/api/v2/pokemon/${el}`);
+
+      return apiCall
+    });
+
+    Promise.all(promises).then(result => setPokemon(result));
+
+  }, []);
+
+  console.log(pokemon)
+
+  
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className="main">
+        
+        <Card></Card>
+     </div>
     </>
   )
 }
---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card ({imageSource, name}){
+
+    return (
+        <div className="card">
+            <p>Sprite</p>
+            <p>Nome Pokemon</p>
+        </div>
+    )
+}
+
+export default Card;
